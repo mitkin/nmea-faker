2ab37a82a4796332adf3d755b83c2dc7544c03a4
Mikhail Itkin <itkin.m@gmail.com>
2023-05-31 15:48:23 +0200

    remove redundant blocks

--- main.go
+++ main.go
@@ -10,32 +10,30 @@ import (
 	"time"
 )
 
-func handleConnection(connection net.Conn, messages [][]string) {
+func handleConnection(connection net.Conn, messages []string) {
 	defer connection.Close()
 
 	for {
-		for _, block := range messages {
-			for _, message := range block {
-				// Generate a random float between 0 and 360 and reduce the range to 60 degrees
-				bearing := rand.Float64() * 360 / 6.
-				fmt.Println("Sending message:", modifyBearing(message, bearing))
-				modifiedMessage := modifyBearing(message, bearing)
-				_, err := connection.Write([]byte(modifiedMessage + "\n"))
+		for _, message := range messages {
+			// Generate a random float between 0 and 360 and reduce the range to 60 degrees
+			bearing := rand.Float64() * 360 / 6.
+			fmt.Println("Sending message:", modifyBearing(message, bearing))
+			modifiedMessage := modifyBearing(message, bearing)
+			_, err := connection.Write([]byte(modifiedMessage + "\n"))
 
-				if err != nil {
-					netErr, ok := err.(*net.OpError)
-					if ok && netErr.Err.Error() == "broken pipe" {
-						fmt.Println("Client disconnected.")
-						return
-					}
-
-					fmt.Println("Error occurred:", err)
+			if err != nil {
+				netErr, ok := err.(*net.OpError)
+				if ok && netErr.Err.Error() == "broken pipe" {
+					fmt.Println("Client disconnected.")
 					return
 				}
 
+				fmt.Println("Error occurred:", err)
+				return
 			}
-			time.Sleep(500 * time.Millisecond)
+
 		}
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
@@ -74,23 +72,6 @@ func main() {
 		"$PSXN,23,0.01,0.12,66.99,0.07*0D",
 	}
 
-	totalMessages := len(nmeaMessages)
-	if totalMessages < 3 {
-		fmt.Println("Insufficient messages to split into three lists")
-		return
-	}
-
-	// Calculate the number of messages in each list
-	listSize := totalMessages / 3
-	remainder := totalMessages % 3
-
-	// Create three separate lists
-	list1 := nmeaMessages[:listSize]
-	list2 := nmeaMessages[listSize : 2*listSize+remainder]
-	list3 := nmeaMessages[2*listSize+remainder:]
-
-	messageList := [][]string{list1, list2, list3}
-
 	listener, err := net.Listen("tcp", tcpIP+":"+strconv.Itoa(tcpPort))
 	if err != nil {
 		panic(err)
@@ -103,7 +84,7 @@ func main() {
 			panic(err)
 		}
 
-		go handleConnection(connection, messageList)
+		go handleConnection(connection, nmeaMessages)
 
 	}
 
